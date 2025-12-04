56d79ca45909492b635e99216b6e285b45a1a83d
Ri9tyochardBlazajckburn <iroida.m.712378481@rambler.ru>
2025-12-04 09:32:54 +0800

    add local.ts, Impl.json, local-test.java

---
+++ frontend/web/src/local.ts
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+export const manager-analytics = () => { return null; };
---
+++ local-test.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\local-test.java
+// Generated at: 2025-10-05T13:03:32.319Z
